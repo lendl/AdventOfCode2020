90ac14ac3217f9ef6db8693f45a430051b23fd7a
lendl <lendlverschoor@hotmail.com>
2021-03-28 18:02:47 +0200

    Add Day1

---
+++ Day1/ReportRepair/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ReportRepair
+{
+    class Program
+    {
+        private static List<int> _entries;
+        static void Main(string[] args)
+        {
+            _entries = ProcessInputFile();
+            PartOne();
+            PartTwo();
+        }
+
+        private static void PartOne()
+        {
+            Console.WriteLine("--- Part One ---");
+            var sw = Stopwatch.StartNew();
+            var result = SearchValue(2);
+            Console.WriteLine($"Solution : {result}");
+            sw.Stop();
+            Console.WriteLine($"Time to calculate Solution : {sw.ElapsedMilliseconds}");
+        }
+
+        private static void PartTwo()
+        {
+            Console.WriteLine("--- Part Two ---");
+            var sw = Stopwatch.StartNew();
+            var result = SearchValue(3);
+            Console.WriteLine($"Solution : {result}");
+            sw.Stop();
+            Console.WriteLine($"Time to calculate Solution : {sw.ElapsedMilliseconds}");
+        }
+
+        private static int SearchValue(int length)
+        {
+            var value = 2020;
+            var entries = GetPossibleCombinations(_entries, length);
+            Console.WriteLine($"Possible combinations found : {entries.Count()}");
+            var values = entries.Single(i => i.Aggregate((i1, i2) => i1 + i2) == value);
+            return values.Aggregate((e1, e2) => e1 * e2);
+        }
+
+        private static IEnumerable<IEnumerable<int>> GetPossibleCombinations(IEnumerable<int> entries, int length) 
+        {
+            if (length == 1)
+            {
+                return entries.Select(t => new [] { t });
+            }
+            return GetPossibleCombinations(entries, length - 1)
+                .SelectMany(t => entries.Where(e => e.CompareTo(t.Last()) > 0),
+                    (t1, t2) => t1.Concat(new [] { t2 }));
+        }
+        
+        private static List<int> ProcessInputFile()
+        {
+            Console.WriteLine("--- Processing Input file ---");
+            var result = new List<int>();
+            string line;
+            var file = new StreamReader(@$"{AppDomain.CurrentDomain.BaseDirectory}input.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                result.Add(int.Parse(line));
+            }
+
+            file.Close();
+            Console.WriteLine($"{result.Count} lines processed");
+            return result;
+        }
+    }
+}
---
+++ Day2/PasswordPhilosophy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PasswordPhilosophy
+{
+    class Program
+    {
+        private static List<Password> _passwords;
+        static void Main(string[] args)
+        {
+            _passwords = ProcessInputFile();
+        }
+    }
+
+    public class Password
+    {
+        public int PolicyMinimal { get; set; }
+        public int PolicyMaximum { get; set; }
+        public string PolicyCharachter { get; set; }
+        public string Value { get; set; }
+    }
+}
